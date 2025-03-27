b23a53bcf22c4257e258f68504e593b5208c634d
JacopoSfolgor1 <j.sfolgori@its-ictacademy.com>
2025-03-27 14:31:47 +0100

    sfg

--- Lezione_6/4ClassAttempt.py
+++ Lezione_6/4ClassAttempt.py
@@ -1,3 +1,12 @@
+'''Exercise 3 (Folder 9 ex_3.py)
+Given is the class Animal. For each task, test your changes!
+1. Create two realistic instances of Animals
+2. Print the name of each object
+3. Change the amount of legs of one object using the dot notation
+4. Add a method setLegs() to set the legs of an object and repeat task 3 but
+this time using the method.
+5. Add a method getLegs() to return the amount of legs
+6. Add a method named printInfo that prints all attributes of the Animal'''
 class Animal:
     
     def __init__(self,name,legs):
@@ -19,9 +28,9 @@ class Animal:
     
 
 dog = Animal("Dog",3)
-bird = Animal("bird",2)
+bird = Animal("Bird",2)
 
-print(f"{dog.name} name animal 1, {bird.name} name animnal 2")
+print(f"{dog.name} name animal 1, {bird.name} name animal 2")
 
 dog.legs = 3
 print(f"{dog.name} has now {dog.legs} legs")
